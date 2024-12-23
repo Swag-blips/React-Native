be6404ad489a448fe90542055eaadbf77fdd6275
Swag-blips <coderblip@gmail.com>
2024-12-23 17:16:55 +0100

    feat:developing splash screen

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          src={images.onboarding}
+          className="w-full"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to ReState
+          </Text>
+          <Text className="text-3xl text-center uppercase font-rubik text-black-200">
+            Let's get you closer to {"\n"}
+            <Text className="text-primary-300">Your Ideal home</Text>
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
