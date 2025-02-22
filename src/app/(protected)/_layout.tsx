ee491dc1bb72acb26f281a74c66e007fd29a0ac4
Swag-blips <coderblip@gmail.com>
2025-02-22 16:18:44 +0100

    feat:clerk auth setup complete

---
+++ src/app/(protected)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack, Redirect } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
+export default function AppLayout() {
+  const { isSignedIn } = useAuth();
+
+  if (!isSignedIn) {
+    return <Redirect href={"/signIn"} />;
+  }
+  return <Stack />;
+}
