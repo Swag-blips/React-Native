6935289ede230d630d907d8da91477bd6951fb20
Bakrin Olasupo <coderblip@gmail.com>
2026-04-03 13:19:36 +0100

    feat:reset RN repo

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,4 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -11,16 +10,6 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href={"/about"}>Visit about page</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "red",
-  },
-});
